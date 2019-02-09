65ae619b68ff4eaabaab6d0358d60054e84158a7
revK <revenoir7@gmail.com>
2019-02-09 17:23:16 +0900

    계산기 구현하기2
    숫자 2개 입력받아서 사칙연산 출력하기(input태그 이용)

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,8 +1,20 @@
-function main() {
-    console.log(6 + " + " + 3 + " = " + (6+3));
-    console.log(6 + " - " + 3 + " = " + (6-3));
-    console.log(6 + " * " + 3 + " = " + (6*3));
-    console.log(6 + " / " + 3 + " = " + (6/3));
+var number = {};
+number.inputs = [];
+
+var cal = function() {
+    readInput();
+    console.log(number.inputs[0] + number.inputs[1]);
+    console.log(number.inputs[0] - number.inputs[1]);
+    console.log(number.inputs[0] * number.inputs[1]);
+    console.log(number.inputs[0] / number.inputs[1]);
 }
 
-main();
+var readInput = function() {
+    var n1 = document.getElementById('input1');
+    var n2 = document.getElementById('input2');
+    var num1 = Number(n1.value);
+    var num2 = Number(n2.value);
+    console.log(num1, num2);
+    number.inputs.push(num1, num2);
+    console.log(number.inputs);
+}
