822ce097fbe3e88551f9420b70d1dafbb19dd2e7
revK <revenoir7@gmail.com>
2019-02-12 02:23:03 +0900

    조건문을 활용한 계산기 구현

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,20 +1,34 @@
-var number = {};
-number.inputs = [];
+var inputs = [];
 
 var cal = function() {
     readInput();
-    console.log(number.inputs[0] + number.inputs[1]);
-    console.log(number.inputs[0] - number.inputs[1]);
-    console.log(number.inputs[0] * number.inputs[1]);
-    console.log(number.inputs[0] / number.inputs[1]);
+    var result = document.getElementById('result');
+    var str = ""
+    if(inputs[2] === '+') {
+        str += "더하기 : " + (inputs[0] + inputs[1]) + "<br>";
+        result.innerHTML = str;
+    } else if(inputs[2] === '-') {
+        str += "빼기 : " + (inputs[0] - inputs[1]) + "<br>";
+        result.innerHTML = str;
+    } else if(inputs[2] === '*') {
+        str += "곱하기 : " + (inputs[0] * inputs[1]) + "<br>";
+        result.innerHTML = str;
+    } else if(inputs[2] === '/') {
+        str += "나누기 : " + (inputs[0] / inputs[1]) + "<br>";
+        result.innerHTML = str;
+    }
 }
 
 var readInput = function() {
+    inputs = [];
     var n1 = document.getElementById('input1');
     var n2 = document.getElementById('input2');
+    var s0 = document.getElementById('input3');
     var num1 = Number(n1.value);
     var num2 = Number(n2.value);
-    console.log(num1, num2);
-    number.inputs.push(num1, num2);
-    console.log(number.inputs);
+    var symbol = s0.value;
+    console.log(num1, num2, symbol);
+    inputs.push(num1, num2, symbol);
+    console.log(inputs);
 }
+
