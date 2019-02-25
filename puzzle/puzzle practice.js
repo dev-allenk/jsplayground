a749a46a10cda7a96e81dedaeb78a86f70b85e96
revK <revenoir7@gmail.com>
2019-02-25 18:07:36 +0900

    밀어내기 퍼즐 구현 연습
    시간 측정 기능 구현

--- puzzle/puzzle practice.js
+++ puzzle/puzzle practice.js
@@ -2,7 +2,7 @@
 const game = {
     text: ['HELLO', 'NICE TO', 'MEET', 'YOU', 'HAVE', 'NICE', 'WONDERFUL', 'BEAUTIFUL', 'DAY', 'LOVE'],
     quest: [],
-    btns: []
+    btns: [],
 };
 //문제 텍스트를 랜덤하게 추출하는 메소드
 //text배열에서 랜덤하게 단어를 골라서 btns배열에 한글자씩 넣고 id=text부분에 출력 비교를 위해 quest배열에 단어넣기
@@ -40,8 +40,10 @@ game.newGame = function () {
 //다시시작하는 메소드
 game.restart = function () {
     this.newGame();
+    game.startTime = Date.now();
     output.hideShowBtns('show');
     output.countArea.innerHTML = '';
+    output.countTimeArea.innerHTML = '';
     output.answer = 0;
 }
 
@@ -83,12 +85,14 @@ action.shuffle = function () {
 //화면 출력을 담당하는 객체
 const output = {
     answer: 0,
+    maxPlay: 3,
     countArea: document.getElementById('count'),
     textArea: document.getElementById('text'),
     textBtnArea: document.getElementById('textBtn'),
     actionBtnsArea: document.getElementById('actionBtns'),
     restartArea: document.getElementById('restart'),
-    resultArea: document.getElementById('result')
+    resultArea: document.getElementById('result'),
+    countTimeArea: document.getElementById('countTime')
 };
 
 //문제 텍스트를 출력하는 메소드
@@ -120,7 +124,9 @@ output.countAnswer = function () {
 
 //세문제 맞추면 종료문구 출력하는 메소드
 output.printThks = function () {
-    if (this.answer === 3) {
+    if (this.answer === this.maxPlay) {
+        game.endTime = Date.now();
+        this.countTime(game.startTime, game.endTime);
         this.countArea.innerHTML = 'Thank you for playing!';
         this.hideShowBtns('hide')
     } else {
@@ -128,7 +134,7 @@ output.printThks = function () {
     }
 }
 
-//액션버튼 숨기는 메소드
+//액션버튼 숨기거나 나타내는 메소드
 output.hideShowBtns = function (status) {
     if (status === 'hide') {
         this.actionBtnsArea.style.display = 'none';
@@ -139,5 +145,12 @@ output.hideShowBtns = function (status) {
     }
 }
 
+//문제 푸는데 걸린 시간을 측정하고 표시하는 메소드
+output.countTime = function (t1, t2) {
+    let playTime = t2 - t1;
+    this.countTimeArea.innerHTML = playTime / 1000 + '초';
+}
+
 //첫 게임 시작
 game.newGame();
+game.startTime = Date.now();
